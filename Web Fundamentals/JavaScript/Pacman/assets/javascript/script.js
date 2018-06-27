68aad4e30fa0fc381d555bef35c7628e99030747
Jerryco <jerrycomortiz457>
2018-06-26 18:50:24 -0700

    pacman update

--- Web Fundamentals/JavaScript/Pacman/assets/javascript/script.js
+++ Web Fundamentals/JavaScript/Pacman/assets/javascript/script.js
@@ -3,35 +3,38 @@
 // 2) Have the pacman move up and down
 
 var world = [
-    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
-    [2,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
-    [2,1,1,2,2,1,1,2,2,1,1,2,2,2,1,1,2,1,2,2,1,1,1,2,2,1,1,1,1,1,2],
-    [2,1,2,1,1,1,1,1,1,2,1,2,1,1,2,1,1,1,2,1,2,1,2,1,1,1,1,1,1,1,2],
-    [2,1,2,1,1,1,2,1,1,2,1,1,1,1,2,1,2,1,2,1,2,1,2,1,2,2,1,1,1,1,2],
-    [2,1,2,1,1,1,2,1,1,2,1,2,1,1,2,1,2,1,2,1,2,1,2,1,1,2,1,1,1,1,2],
-    [2,1,1,2,2,1,1,2,2,1,1,2,2,2,1,1,2,1,2,1,2,1,1,2,2,1,1,1,1,1,2],
-    [2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
-    [2,1,2,2,2,1,1,1,2,2,1,1,1,1,1,2,1,1,2,2,1,1,1,2,1,1,2,2,1,1,2],
-    [2,1,2,1,1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1,2,1,1,2,1,1,2,1,2],
-    [2,1,1,1,1,2,1,2,1,1,2,1,2,1,1,2,1,2,1,1,2,1,1,2,1,1,1,2,1,1,2],
-    [2,1,2,1,1,2,1,2,1,1,2,1,2,1,1,2,1,2,1,1,2,1,1,1,2,1,2,1,1,1,2],
-    [2,1,2,2,2,1,1,1,2,2,1,1,1,2,2,1,1,1,2,2,1,1,1,1,1,2,1,1,1,1,2],
-    [2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],   
-    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+    [4,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4],
+    [4,1,1,5,5,1,1,2,2,1,1,5,5,5,1,1,2,1,5,5,1,1,1,2,2,1,1,1,1,1,4],
+    [4,1,5,1,1,1,1,1,1,2,1,5,1,1,5,1,1,1,5,1,5,1,2,1,1,1,1,1,1,1,4],
+    [4,1,5,1,1,1,2,1,1,2,1,1,1,1,5,1,2,1,5,1,5,1,2,1,2,2,1,1,1,1,4],
+    [4,1,5,1,1,1,2,1,1,2,1,5,1,1,5,1,2,1,5,1,5,1,2,1,1,2,1,1,1,1,4],
+    [4,1,1,5,5,1,1,2,2,1,1,5,5,5,1,1,2,1,5,1,5,1,1,2,2,1,1,1,1,1,4],
+    [4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4],
+    [4,1,2,2,2,1,1,1,5,5,1,1,1,1,1,2,1,1,5,5,1,1,1,2,1,1,2,2,1,1,4],
+    [4,1,2,1,1,2,1,1,1,1,5,1,1,1,1,2,1,1,1,1,5,1,2,1,1,2,1,1,2,1,4],
+    [4,1,1,1,1,2,1,5,1,1,5,1,2,1,1,2,1,5,1,1,5,1,1,2,1,1,1,2,1,1,4],
+    [4,1,2,1,1,2,1,5,1,1,5,1,2,1,1,2,1,5,1,1,5,1,1,1,2,1,2,1,1,1,4],
+    [4,1,2,2,2,1,1,1,5,5,1,1,1,2,2,1,1,1,5,5,1,1,1,1,1,2,1,1,1,1,4],
+    [4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4],   
+    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]
     
     
 ];
 
 var score = 0;
+var num = 300;
 var pacman = {
     x: 1,
     y: 1
 };
+
+var cyanghost = { x: 13,y: 4 }, redghost = { x: 4,y: 10 }, pinkghost = { x: 3,y: 10 }, orangeghost = { x: 12,y: 4 } ;
+
 var cherry = {
     x:15,
     y:7
 }
-
 
 function displayWorld() {
     var output = '';
@@ -39,7 +42,7 @@ function displayWorld() {
         output += "<div class='row'>";
         for (var j = 0; j < world[i].length; j++) {
             if (world[i][j] == 2)
-                output += "<div class='brick'></div>";
+                output += "<div class='redwall'></div>";
             if (world[i][j] == 1)
                 output += "<div class='coin'></div>";
             if (world[i][j] == 0)
@@ -47,7 +50,9 @@ function displayWorld() {
             if (world[i][j] == 3)
                 output += "<div class='cherry'></div>";
             if (world[i][j] == 4)
-                output += "<div class='cherryTree'></div>";
+                output += "<div class='brick'></div>";
+            if (world[i][j] == 5)
+                output += "<div class='bluewall'></div>";
         }
         output += "</div>";
     }
@@ -67,21 +72,38 @@ function displayCherry(){
     world[cherry.y][cherry.x] = 3; 
 }
 
+function summonGhost(){
+    document.getElementById('cyanghost').style.top = cyanghost.y*20+"px";
+    document.getElementById('cyanghost').style.left = cyanghost.x*20+"px";
+    document.getElementById('redghost').style.top = redghost.y*20+"px";
+    document.getElementById('redghost').style.left = redghost.x*20+"px";
+    document.getElementById('pinkghost').style.top = pinkghost.y*20+"px";
+    document.getElementById('pinkghost').style.left = pinkghost.x*20+"px";
+    document.getElementById('orangeghost').style.top = orangeghost.y*20+"px";
+    document.getElementById('orangeghost').style.left = orangeghost.x*20+"px";
+}
+
 displayWorld();
 displayPacman();
+summonGhost();
 
 document.onkeydown = function(e){
-    if(e.keyCode == 37 && world[pacman.y][pacman.x-1] != 2){
+    if(e.keyCode == 37 && world[pacman.y][pacman.x-1] != 2 && world[pacman.y][pacman.x-1] != 5 && world[pacman.y][pacman.x-1] != 4){
        pacman.x--;
+       document.getElementById("pacman").style.transform = "rotate(180deg)";
     }
-    else if (e.keyCode == 39 && world[pacman.y][pacman.x+1] != 2){
+   
+    else if (e.keyCode == 39 && world[pacman.y][pacman.x+1] != 2 && world[pacman.y][pacman.x+1] != 5 && world[pacman.y][pacman.x+1] != 4){
         pacman.x++;
+        document.getElementById("pacman").style.transform = "rotate(0deg)";
     }
-    else if (e.keyCode == 40 && world[pacman.y+1][pacman.x] != 2){
+    else if (e.keyCode == 40 && world[pacman.y+1][pacman.x] != 2 && world[pacman.y+1][pacman.x] != 5 && world[pacman.y+1][pacman.x] != 4){
         pacman.y++;
+        document.getElementById("pacman").style.transform = "rotate(90deg)";
     }
-    else if (e.keyCode == 38 && world[pacman.y-1][pacman.x] != 2){
+    else if (e.keyCode == 38 && world[pacman.y-1][pacman.x] != 2 && world[pacman.y-1][pacman.x] != 5 && world[pacman.y-1][pacman.x] != 4){
         pacman.y--;
+        document.getElementById("pacman").style.transform = "rotate(270deg)";
     }
     if(world[pacman.y][pacman.x] == 1){
         world[pacman.y][pacman.x] = 0;
@@ -95,9 +117,9 @@ document.onkeydown = function(e){
         displayWorld();
         displayScore();
     }
-    if (score != ){
-        num =                      
-        displayCherry();
+    if (num == score || num < score){
+        num = num + 300;       
+        displayCherry();        
     }
        // console.log(e.keyCode);
     displayPacman();   
