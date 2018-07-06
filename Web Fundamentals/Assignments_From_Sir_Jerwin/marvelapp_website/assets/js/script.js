98be017ed9011ba3275a382d90ef341a4d802eb8
jerrycomortiz457 <jerryco.m.ortiz.457@gmail.com>
2018-07-06 10:22:13 -0700

    paginationhere update in marvelapp.com

--- Web Fundamentals/Assignments_From_Sir_Jerwin/marvelapp_website/assets/js/script.js
+++ Web Fundamentals/Assignments_From_Sir_Jerwin/marvelapp_website/assets/js/script.js
@@ -1,28 +1,19 @@
+function reset(e) {
+    $(e).children().parent().siblings().children('div').css('color', 'black');
+    $(e).children().parent().siblings().children('svg').css('fill', 'none');
+}
+function cssupdate(e) {
+    $(e).children('div').css('color', '#1FB6FF');
+    $(e).children('svg').css('fill', '1FB6FF');
+}
 $(document).ready(function () {
-    $('.display').click(function () {
-        var design = ($('#target_image').attr('data-alt-src'));
-        var prototype = ($('#target_image').attr('data-alt-src1'));
-        var handoff = ($('#target_image').attr('data-alt-src2'));
-        var collaboration = ($('#target_image').attr('data-alt-src3'));
-
-        if ($(this).attr('class', dp1)) {
-            $('#target_image').attr('src', design);
-        }
-        if ($(this).attr('class', dp2)) {
-            $('#target_image').attr('src', prototype);
-        }
-        if ($(this).attr('class', dp3)) {
-            $('#target_image').attr('src', handoff);
-        }
-        if ($(this).attr('class', dp4)) {
-            $('#target_image').attr('src', collaboration);
-        }
-        // if ($(this).attr('class') != $('.displayd').attr('data-alt-src')) {
-        //     
-        // }
-        // if ($(this).attr('src') != $('#target_image').attr('data-alt-src')) {
-        //     $('#target_image').attr('src', ninja);
-        // }
+    $(document).on("click", ".displaythis", function () {
+        var idcode = $(this).attr('id');
+        var currentsrc = '../assets/images/' + idcode + '.jpg';
+        $('#displayhere').attr("src", currentsrc);
+        console.log("clicked", + idcode)
+        cssupdate(this);
+        reset(this);
 
     });
 });
